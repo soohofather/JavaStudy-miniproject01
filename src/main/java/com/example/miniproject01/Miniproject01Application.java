7a49390ba7620462e5474ff569fbd7d2a00561f5
이동현 <enthchal@gmail.com>
2024-05-08 08:27:57 +0900

    first commit

---
+++ src/main/java/com/example/miniproject01/Miniproject01Application.java
@@ -0,0 +1,13 @@
+package com.example.miniproject01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Miniproject01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Miniproject01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/miniproject01/Miniproject01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.miniproject01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Miniproject01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
