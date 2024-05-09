3c63170b0502c620dc18bd15765f58d1f3046d35
이동현 <enthchal@gmail.com>
2024-05-09 16:03:58 +0900

    Feat: 검색 기능 추가

--- src/main/java/com/example/miniproject01/movie/db/MovieRepository.java
+++ src/main/java/com/example/miniproject01/movie/db/MovieRepository.java
@@ -1,6 +1,13 @@
 package com.example.miniproject01.movie.db;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface MovieRepository extends JpaRepository<MovieEntity, Long> {
+
+    @Query("SELECT m FROM movie m WHERE LOWER(m.title) LIKE LOWER(CONCAT('%', :title, '%'))")
+    List<MovieEntity> findByTitleContaining(@Param("title") String title);
 }
